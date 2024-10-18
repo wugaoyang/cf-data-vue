2d7989bd4b8b899d16b71b161cbe348bbb5deeef
Leo <wugaoyang0@gmail.com>
2024-10-18 21:57:53 +0800

    UPDATE

--- src/views/bestip/list.vue
+++ src/views/bestip/list.vue
@@ -1,32 +1,62 @@
 <template>
   <div class="query">
-    <el-input
-        v-model="name"
-        style="max-width: 600px; width: 300px"
+    <span><el-input
+        v-model="queryData.name"
+        style="max-width: 600px; width: 200px"
         placeholder="请输入名字"
         @keyup="getIpList"
     >
       <template #prepend>名字</template>
     </el-input>
+      </span>
+    <span>
     <el-input
-        v-model="group"
-        style="max-width: 600px; width: 300px"
+        v-model="queryData.group"
+        style="max-width: 600px; width: 200px"
         placeholder="请输入分组"
         @keyup="getIpList"
     >
       <template #prepend>分组</template>
     </el-input>
+      </span>
+    <span>
+    <el-input
+        v-model="queryData.countryCode"
+        style="max-width: 600px; width: 200px"
+        placeholder="请输入国家代码"
+        @keyup="getIpList"
+    >
+      <template #prepend>国家代码</template>
+    </el-input>
+    </span>
+    <span>
     <el-select
-        v-model="status"
-        style="max-width: 600px; width: 300px"
+        v-model="queryData.status"
+        style="max-width: 600px; width: 150px"
         @change="getIpList"
     >
       <el-option label="" value=""></el-option>
       <el-option label="启用" value="1">启用</el-option>
       <el-option label="禁用" value="0">禁用</el-option>
-      <template #prepend>状态</template>
+      <template #prefix>状态</template>
     </el-select>
+    </span>
+
+    <span>
+    <el-select
+        v-model="queryData.reachable"
+        style="max-width: 600px; width: 150px"
+        @change="getIpList"
+    >
+      <el-option label="" value=""></el-option>
+      <el-option label="是" value="1">是</el-option>
+      <el-option label="否" value="0">否</el-option>
+      <template #prefix>可达</template>
+    </el-select>
+    </span>
     <el-button type="primary" @click="getIpList">查询</el-button>
+
+    <el-checkbox v-model="autoRefresh">自动刷新</el-checkbox>
   </div>
   <el-table :data="bestIpList" style="width: 100%" :height="500" v-loading="loading">
     <el-table-column type="index" width="50"/>
@@ -34,8 +64,9 @@
     <el-table-column prop="name" label="名字" width="180"/>
     <el-table-column prop="group" label="分组"/>
     <el-table-column prop="cityNameCN" label="城市"/>
-    <el-table-column prop="countryCode" label="国家代码"/>
+    <el-table-column prop="cityNameEN" label="城市"/>
     <el-table-column prop="countryNameCN" label="国家名字"/>
+    <el-table-column prop="countryCode" label="国家代码"/>
     <el-table-column prop="source" label="来源"/>
     <el-table-column prop="delay" label="延迟(ms)"/>
     <el-table-column prop="speed" label="速度(MB/s)"/>
@@ -76,21 +107,33 @@ const pageVO = reactive({
   pageSize: 10
 })
 
+const queryData = reactive({
+  name: '',
+  group: '',
+  status: '',
+  countryCode: '',
+  reachable: ''
+})
+
 const loading = ref(true)
-const name = ref("")
-const group = ref("")
-const status = ref('')
 const bestIpList = ref([])
+const autoRefresh = ref(false)
 
 getIpList()
 
+setInterval(()=>{
+  if (autoRefresh.value && !loading.value){
+    getIpList();
+  }
+}, 1000)
+
 function getIpList() {
   loading.value = true
-  let queryData = {
-    data: {name: name.value, group: group.value, status: status.value},
+  let query = {
+    data: queryData,
     pageVO: pageVO
   }
-  BestIpApi.page(queryData).then(res => {
+  BestIpApi.page(query).then(res => {
     bestIpList.value = res.data.data
     pageVO.total = res.data.total
     loading.value = false
@@ -99,4 +142,7 @@ function getIpList() {
 
 </script>
 <style>
+.query span {
+  margin: 5px;
+}
 </style>
