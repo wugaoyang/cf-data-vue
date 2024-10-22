7122170485175775981b9825992fcc7d84a97c52
Leo <wugaoyang0@gmail.com>
2024-10-22 21:32:22 +0800

    UPDATE

---
+++ src/api/CfIpFavoriteApi.ts
@@ -0,0 +1,51 @@
+import createAxios from '@/utils/axios'
+import {h} from 'vue'
+import {ElNotification} from "element-plus";
+
+export const url = '/api/db/ip/favorite'
+
+export default class CfIpFavoriteApi {
+    static getBestIp() {
+        return createAxios(
+            {
+                url: url + '/best',
+                method: 'get',
+            }
+        )
+    }
+
+    static page(data: any) {
+        return createAxios(
+            {
+                url: url + '/page',
+                method: 'post',
+                data: data
+            }
+        )
+    }
+
+    static list(data: any) {
+        return createAxios(
+            {
+                url: url + '/list?limit=20',
+                method: 'get',
+            }
+        )
+    }
+
+    static update(data: object, group: any, deleteOld: any) {
+        return createAxios(
+            {
+                url: url + '/update?group=' + group + '&deleteOld=' + deleteOld,
+                method: 'post',
+                data: data
+            }
+        ).then(res => {
+            console.log("响应", res)
+            ElNotification({
+                title: '更新提示',
+                message: h('i', {style: 'color: teal'}, res.data),
+            })
+        })
+    }
+}
--- src/router/index.ts
+++ src/router/index.ts
@@ -34,6 +34,15 @@ const router = createRouter({
             // this generates a separate chunk (About.[hash].js) for this route
             // which is lazy-loaded when the route is visited.
             component: () => import('@/views/ip/update.vue')
+        },
+        {
+            path: '/favorite/list',
+            name: 'ipFavoriteList',
+            meta: {title: 'CF IP库'},
+            // route level code-splitting
+            // this generates a separate chunk (About.[hash].js) for this route
+            // which is lazy-loaded when the route is visited.
+            component: () => import('@/views/favorite/list.vue')
         }
     ]
 })
